52ffecb4f048a6ecca5295289c643ffc67efc6a2
yuumairie <rujiangyoumo42@gmail.com>
2020-05-03 20:25:42 +0900

    new

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>掲示板に投稿する</h3>
+    <label for="name">ニックネーム</label>
+    <input type="text" id="name" v-model="name" />
+    <br />
+    <br />
+    <label for="comment">コメント</label>
+    <textarea id="comment" v-model="comment"></textarea>
+    <br />
+    <br />
+    <button @click="createComment">コメントをサーバーに送る</button>
+    <h2>掲示板</h2>
+    <div v-for="post in posts" :key="post.name">
+      <div>名前:{{post.fields.name.stringValue}}</div>
+      <div>コメント:{{post.fields.comment.stringValue}}</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: "",
+      comment: "",
+      posts: []
+    };
+  },
+  created() {
+    axios
+      .get(
+        "https://firestore.googleapis.com/v1/projects/vuejs-http-9d94e/databases/(default)/documents/comments"
+      )
+      .then(response => {
+        this.posts = response.data.documents;
+      });
+  },
+  methods: {
+    createComment() {
+      axios.post(
+        "https://firestore.googleapis.com/v1/projects/vuejs-http-9d94e/databases/(default)/documents/comments",
+        {
+          fields: {
+            name: {
+              stringValue: this.name
+            },
+            comment: {
+              stringValue: this.comment
+            }
+          }
+        }
+      );
+
+      this.name = "";
+      this.comment = "";
+    }
   }
-}
+};
 </script>
+
 
 <style>
 #app {
